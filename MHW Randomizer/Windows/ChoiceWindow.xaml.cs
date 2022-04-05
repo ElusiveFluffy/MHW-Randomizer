1bc6fbd9c7acb9b071f8e669624bc4ae4857ad0d
ElusiveFluffy <Freya8822@hotmail.com>
2022-04-05 22:03:17 +0930

    Rearranged some files to folders that better match their purpose

---
+++ MHW Randomizer/Windows/ChoiceWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace MHW_Randomizer
+{
+    /// <summary>
+    /// Interaction logic for MessageWindow.xaml
+    /// </summary>
+    public partial class ChoiceWindow : Window
+    {
+        public ChoiceWindow(string message)
+        {
+            InitializeComponent();
+            MessageTB.Text = message;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
