6750601d02ca3b6e40a9e1e7d5eb2cca7a6efe0a
ElusiveFluffy <Freya8822@hotmail.com>
2022-04-21 20:14:13 +0930

    Added monster tab

---
+++ MHW Randomizer/Controls/Monster.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MHW_Randomizer
+{
+    /// <summary>
+    /// Interaction logic for Monster.xaml
+    /// </summary>
+    public partial class Monster : UserControl
+    {
+        public Monster()
+        {
+            InitializeComponent();
+        }
+    }
+}
